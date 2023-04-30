8f6de219a2488409a09a654f2e22527487b76339
Ayu327 <ayusaini327@gmail.com>
2023-04-30 19:31:08 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Header from "./component/Header";
 import Home from "./component/Home";
 import Footer from "./component/Footer";
+import Contact from "./component/Contact";
+import Service from "./component/Service";
 import "./styles/App.scss"
 import "./styles/Header.scss"
 import "./styles/Footer.scss"
 import "./styles/contact.scss"
-import Contact from "./component/Contact";
+import "./styles/mediaQueries.scss"
+
+
 function App() {
   return (
  <Router>
@@ -15,6 +19,7 @@ function App() {
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/contact" element={<Contact/>}/>
+    <Route path="/service" element={<Service/>}/>
 
   </Routes>
 <Footer/>
